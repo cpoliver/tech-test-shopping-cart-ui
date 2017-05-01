ee68113480dbad2a188cf81ca75e4f4509535131
Charles P. Oliver <cpoliver@gmail.com>
2017-05-01 19:41:47 +0200

    Add basic item quantity component

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 
 import './app.css';
 
+import ItemQuantity from '../item-quantity'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ItemQuantity item="apple" />
+        <ItemQuantity item="orange" />
+        <ItemQuantity item="garlic" />
+        <ItemQuantity item="papaya" />
       </div>
     );
   }
---
+++ src/components/item-quantity/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './item-quantity.css';
+
+import { APPLE, ORANGE, GARLIC, PAPAYA } from '../../constants';
+
+class ItemQuantity extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: props.count };
+  }
+
+  decrementCount = () => {
+    if (this.state.count > 0) {
+      this.setState(function(prevState, props) {
+        return { count: prevState.count - 1 };
+      });
+    }
+  };
+
+  incrementCount = () => {
+    this.setState(function(prevState, props) {
+      return { count: prevState.count + 1 };
+    });
+  };
+
+  render() {
+    const { item } = this.props;
+    const icon = require(`../../../public/icons/${item}.svg`);
+
+    return (
+      <div className="c-item-quantity">
+        <div className="c-item-quantity__icon">
+          <img src={icon} alt={item} />
+        </div>
+        <div className="c-item-quantity__plusminus">
+          <div className="c-item-quantity__plus" onClick={this.incrementCount}>
+            <span>&#x25B2;</span>
+          </div>
+          <div className="c-item-quantity__count">{this.state.count}</div>
+          <div className="c-item-quantity__minus" onClick={this.decrementCount}>
+            <span>&#x25BC;</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ItemQuantity.defaultProps = {
+    count: 0
+};
+
+ItemQuantity.propTypes = {
+    item: PropTypes.oneOf([ APPLE, ORANGE, GARLIC, PAPAYA ]).isRequired,
+    count: PropTypes.number.isRequired
+};
+
+export default ItemQuantity;
